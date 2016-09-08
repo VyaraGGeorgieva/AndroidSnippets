b64edee1ab70ca238f7684715a55cff66a765a4f
VyaraGGeorgieva <viara.georgieva.g@gmail.com>
2016-09-08 10:23:23 +0300

    Test- question with data and title

--- app/src/main/java/gergieva/vyara/androidsnippets/MainActivity.java
+++ app/src/main/java/gergieva/vyara/androidsnippets/MainActivity.java
@@ -3,7 +3,6 @@ package gergieva.vyara.androidsnippets;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -15,19 +14,17 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView title;
-    private TextView info;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        title = (TextView)findViewById(R.id.title);
-        info = (TextView) findViewById(R.id.info);
+
 
         //Get Data From Text Resource File Contains Json Data.
-        InputStream inputStream = getResources().openRawResource(R.raw.cats_array);
+        InputStream inputStream = getResources().openRawResource(R.raw.questions);
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
         int ctr;
@@ -42,29 +39,35 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         Log.v("Text Data", byteArrayOutputStream.toString());
+
         try {
             // Parse the data into jsonobject to get original data in form of json.
             JSONObject jObject = new JSONObject(
                     byteArrayOutputStream.toString());
 
-            JSONObject jObjectResult = jObject.getJSONObject("Categories");
-            JSONArray jArray = jObjectResult.getJSONArray("Category");
-            String cat_Id = "";
-            String cat_name = "";
+            JSONObject jObjectResult = jObject.getJSONObject("Questions");
+            JSONArray jArray = jObjectResult.getJSONArray("Question");
+            String quest_id = "";
+            String quest_title = "";
+            String quest_info = "";
 
             ArrayList<String[]> data = new ArrayList<String[]>();
             for (int i = 0; i < jArray.length(); i++) {
-                cat_Id = jArray.getJSONObject(i).getString("cat_id");
-                cat_name = jArray.getJSONObject(i).getString("cat_name");
-                Log.v("Cat ID", cat_Id);
-                Log.v("Cat Name", cat_name);
-                data.add(new String[]{cat_Id, cat_name});
+                quest_id = jArray.getJSONObject(i).getString("quest_id");
+                quest_title = jArray.getJSONObject(i).getString("quest_title");
+                quest_info= jArray.getJSONObject(i).getString("quest_info");
 
-                info.setText(cat_name);
-                title.setText(cat_Id);
+                Log.v("Qest id", quest_id);
+                Log.v("Quest title", quest_title);
+                Log.v("Quest info", quest_info);
+
+                data.add(new String[]{quest_id, quest_title,quest_info});
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
     }
 }
